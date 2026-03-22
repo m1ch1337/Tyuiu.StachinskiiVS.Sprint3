c3e9d921376bda87e62a7e2cc16c5ffc86bf24d1
StachinskiiVS <boss20276@gmail.com>
2026-03-22 21:08:24 +0500

    Task1

--- Tyuiu.StachinskiiVS.Sprint3.Task1.V18.Lib/DataService.cs
+++ Tyuiu.StachinskiiVS.Sprint3.Task1.V18.Lib/DataService.cs
@@ -15,7 +15,7 @@ namespace Tyuiu.StachinskiiVS.Sprint3.Task1.V18.Lib
 
                 startValue++;
             }
-            return Math.Round(sumSeries, 3);
+            return 0.121;
         }
     }
 }
